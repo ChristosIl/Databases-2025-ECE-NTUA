f01eff8b530afc826555e817fa93bfb29635b0d3
ChristosIl <chrisiliak1000@gmail.com>
2025-04-11 01:20:06 +0300

    created trigger for after demand insert

--- triggers.sql
+++ triggers.sql
@@ -115,6 +115,7 @@ BEGIN
             FROM Resale_Buyer
             WHERE buyer_id = v_buyer_id;
 
+
             /*Get the new visitor_id*/
             SET new_visitor_id = LAST_INSERT_ID();
 
@@ -147,6 +148,8 @@ BEGIN
         );
 
         /*Σβήσε τις εγγραφές από queues*/
+        /*ΔΕΝ ΜΠΟΡΕΙΣ ΝΑ ΚΑΝΕΙΣ UPDATE ΤΟ TABLE ΠΟΥ ΧΡΗΣΙΜΟΠΟΙΕΙ ΤΟ TRIGGER ΟΤΑΝ ΚΑΛΕΙΤΑΙ*/
+        /*UPDATE Resale_Queue SET status = TRUE WHERE resale_id = NEW.resale_id;*/
         UPDATE Demand_Queue SET status = TRUE WHERE demand_id = v_demand_id;
         DELETE FROM Resale_Buyer WHERE buyer_id = v_buyer_id;
     END IF;
@@ -155,6 +158,88 @@ END;
 
 //
 
+CREATE TRIGGER trg_after_demand_queue_insert
+AFTER INSERT ON Demand_Queue
+FOR EACH ROW
+BEGIN 
+    DECLARE v_resale_id INT;
+    DECLARE v_seller_id INT;
+    DECLARE ticket_count INT;
+    DECLARE new_visitor_id INT;
+    DECLARE v_ticket_id INT;
+    DECLARE v_event_id INT;
+    DECLARE v_ticket_type INT;
+    DECLARE resale_price FLOAT;
+
+    /*Matching*/
+    SELECT rq.resale_id, rq.seller_id, rq.ticket_id, rq.price
+    INTO v_resale_id, v_seller_id, v_ticket_id, resale_price
+    FROM Resale_Queue rq
+    JOIN Ticket t ON t.ticket_id = rq.ticket_id
+    WHERE t.event_id = NEW.preferred_event_id
+      AND t.ticket_type_id = NEW.preferred_ticket_type
+      AND rq.status = FALSE
+    LIMIT 1;
+
+    IF v_resale_id IS NOT NULL THEN 
+
+        /*Count how many tickets the seller owns*/
+        SELECT COUNT(*) INTO ticket_count
+        FROM Ticket
+        WHERE visitor_id = v_seller_id;
+
+        IF ticket_count > 1 THEN 
+             /*Create new visitor from Resale_Buyer info*/
+            INSERT INTO Visitor (name, surname, age, email, phone_number, photo_url, photo_description)
+            SELECT name, surname, age, email, phone_number, photo_url, photo_description
+            FROM Resale_Buyer
+            WHERE buyer_id = NEW.buyer_id;
+
+            /*Get the latest visitor id to use it on the ticket*/
+            SET new_visitor_id = LAST_INSERT_ID();
+
+            UPDATE Ticket
+            SET visitor_id = new_visitor_id
+            WHERE ticket_id = v_ticket_id;
+        
+        ELSE  /*Update infos about visitor*/
+            UPDATE Visitor
+            JOIN Resale_Buyer rb ON rb.buyer_id = NEW.buyer_id
+            SET 
+                Visitor.name = rb.name,
+                Visitor.surname = rb.surname,
+                Visitor.age = rb.age,
+                Visitor.email = rb.email,
+                Visitor.phone_number = rb.phone_number,
+                Visitor.photo_url = rb.photo_url,
+                Visitor.photo_description = rb.photo_description
+            WHERE Visitor.visitor_id = v_seller_id;
+        END IF;
+
+        INSERT INTO Resale_Log (ticket_id, old_owner_id, new_owner_id, sale_price)
+        VALUES (
+            v_ticket_id,
+            v_seller_id,
+            IF(ticket_count > 1, new_visitor_id, NEW.buyer_id),
+            resale_price
+
+        );
+
+        
+        /*Σβήσε τις εγγραφές από queues*/
+        /*ΔΕΝ ΜΠΟΡΕΙΣ ΝΑ ΚΑΝΕΙΣ UPDATE ΤΟ TABLE ΠΟΥ ΧΡΗΣΙΜΟΠΟΙΕΙ ΤΟ TRIGGER ΟΤΑΝ ΚΑΛΕΙΤΑΙ
+        ΘΑ ΔΟΚΙΜΑΣΩ ΜΕ BEFORE ΚΑΙ NEW.status := TRUE
+        */
+        UPDATE Resale_Queue SET status = TRUE WHERE resale_id = v_resale_id;
+        DELETE FROM Resale_Buyer WHERE buyer_id = NEW.buyer_id;
+
+    END IF;
+
+
+END;
+
+//
+
 DELIMITER ;
 
-/* Both look to work */
+/* All seem to work. Check the status in resale and demand queue tables */
