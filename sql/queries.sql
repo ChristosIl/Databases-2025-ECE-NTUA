3be25b5808fb3503e065b06efad4e5f82bb2e559
ChristosIl <chrisiliak1000@gmail.com>
2025-04-24 00:26:40 +0300

    query 8

--- sql/queries.sql
+++ sql/queries.sql
@@ -135,3 +135,26 @@ WHERE  DATEDIFF(NOW(), Birth_date) < 10957
 GROUP BY a.artist_id, a.name
 ORDER BY total_festival_participations DESC
 LIMIT 10;
+
+--
+
+-- Query 8 -- 
+-- We chose to present all the support staff and who are available the required date --
+
+SELECT 
+    s.staff_id, 
+    s.name,
+    CASE 
+        WHEN s.staff_id NOT IN (
+            SELECT w.staff_id
+            FROM Works_on w
+            JOIN Event e ON w.event_id = e.event_id
+            WHERE e.event_date = '2025-07-12' /* Required Date*/
+        ) THEN 'YES'
+        ELSE 'NO'
+    END AS available_on_the_Date_we_asked
+FROM Staff s
+WHERE s.staff_role_id = 3;
+
+
+
