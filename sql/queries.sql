210d782f1e61575081e9679bf163728dcc3540ef
ChristosIl <chrisiliak1000@gmail.com>
2025-04-27 20:22:15 +0300

    Query 11

--- sql/queries.sql
+++ sql/queries.sql
@@ -214,9 +214,12 @@ GROUP BY Music_genres
 ORDER BY Number_Appeared DESC
 LIMIT 3;
 
--- Query 11 / To Do! Thats an early stage. Continue Tomorrow-- 
+-- Query 11 -- 
 
-SELECT
+-- Subquery that finds the total participations of artists in a specific year in festivals
+WITH max_participations_per_artist
+AS(
+    SELECT
     a.artist_id, a.name,
     COUNT(*) AS Total_performances
 FROM Artist a 
@@ -225,4 +228,18 @@ JOIN Performance perf ON perf.performs_id = p.performs_id
 JOIN Event e ON e.event_id = perf.event_id
 JOIN Festival f ON f.festival_id = e.festival_id
 WHERE f.year = 2025
-GROUP BY a.artist_id, a.name;
+GROUP BY a.artist_id, a.name
+ORDER BY Total_performances DESC
+)
+
+-- Main query that displays the artists with at least 5 participations less --
+-- than the first artist with max participations --
+SELECT
+    mpa.artist_id, mpa.name,
+    mpa.Total_performances
+FROM max_participations_per_artist mpa
+WHERE (SELECT MAX(Total_performances) FROM max_participations_per_artist) - mpa.Total_performances > 4
+ORDER BY Total_performances DESC;
+
+
+-- Query 12 --
